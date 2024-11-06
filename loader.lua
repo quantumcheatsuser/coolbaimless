39232b58aad4fc4567b4ac7823debdfc37abfe26
Swag12 <164699531+quantumcheatsuser@users.noreply.github.com>
2024-11-06 14:21:59 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,3 +4,26 @@ if game.PlaceId == 2000062521 then
 
 
 end
+
+if game.PlaceId == 1517425836 then
+
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/quantumcheatsuser/coolbaimless/refs/heads/main/stateofanarchy.lua"))()
+
+
+end
+
+if game.PlaceId == 16153158425 then
+
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/quantumcheatsuser/coolbaimless/refs/heads/main/stateofanarchy.lua"))()
+
+
+end
+
+if game.PlaceId == 4026676776 then
+
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/quantumcheatsuser/coolbaimless/refs/heads/main/stateofanarchy.lua"))()
+
+
+end
+
+
